1e57a6ee8ef3531dae358156a2e45d87225a8827
Vadim Blaker <vblaker@gmail.com>
2017-04-06 15:16:34 -0500

    Dictonary change

--- Tutorials/prime_nums.py
+++ Tutorials/prime_nums.py
@@ -1,9 +1,9 @@
- for n in range(2, 10):
+for n in range(2, 10):
     for x in range(2, n):
         if n % x == 0:
             print(n, 'equals', x, '*', n//x)
             break
-    else:
+        else:
         # loop fell through without finding a factor
-        print(n, 'is a prime number')
-        print('Current prime number is %d' % n)
+            print(n, 'is a prime number')
+            print('Current prime number is %d' % n)
