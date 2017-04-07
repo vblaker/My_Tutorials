aca1284d55644ad8a66f18c7437f548eaee9e85c
Vadim Blaker <vblaker@gmail.com>
2017-04-07 08:57:30 -0500

    Strings change

--- Tutorials/visa.py
+++ Tutorials/visa.py
@@ -1,7 +1,14 @@
 import pyvisa
 
 rm = pyvisa.ResourceManager()
-equipment_list = rm.list_resources()
-print(equipment_list)
-inst = rm.open_resource('GPIB0::5::INSTR')
-print(inst.query("*IDN?"))
+equipment_tuple = rm.list_resources()
+print(equipment_tuple)
+equipment_list = list(equipment_tuple)
+print('There are %d items on the list' % len(equipment_list), equipment_list)
+
+for equipment in equipment_list:
+    if 'GPIB' in equipment:
+        instrument = rm.open_resource(equipment)
+        print(instrument.query("*IDN?"), end="")
+    else:
+        print('Equipment %s is not queried' % equipment)
