9a929fe30f01bcf249b3f652953e96c4f633cb76
Vadim Blaker <vblaker@gmail.com>
2017-04-07 13:06:13 -0500

    VISA file changes

--- Tutorials/visa.py
+++ Tutorials/visa.py
@@ -6,9 +6,17 @@ print(equipment_tuple)
 equipment_list = list(equipment_tuple)
 print('There are %d items on the list' % len(equipment_list), equipment_list)
 
+#any('GPIB' in equipment_list for equipment in equipment_list)
+
+i = 0
 for equipment in equipment_list:
     if 'GPIB' in equipment:
         instrument = rm.open_resource(equipment)
         print(instrument.query("*IDN?"), end="")
+        i = i + 1
     else:
         print('Equipment %s is not queried' % equipment)
+        del equipment_list[i]
+
+print('\nNew equipment list is:', equipment_list)
+equipment_tuple = tuple(equipment_list)
