9b078b3f51a11290cb1a693981e60cb954208954
Vadim Blaker <vblaker@gmail.com>
2017-05-02 15:38:50 -0500

    Update sets vs. lists

--- Tutorials/sets.py
+++ Tutorials/sets.py
@@ -1,7 +1,22 @@
-groceries = {'cereal', 'milk', 'crunch', 'beer', 'tape', 'lotion', 'beer'}
-print(groceries)
+groceries_set = {'cereal', 'milk', 'crunch', 'beer', 'tape', 'lotion', 'beer'}
 
-if 'milk' in groceries:
-    print('Milk is there')
+print(groceries_set)
+
+if 'milk' in groceries_set:
+    print('Milk is already there')
 else:
-    print("Oh yeah, you need that")
+    print("Oh yeah, you need milk")
+
+groceries_set.add('one last thing')
+print(groceries_set)
+
+
+#groceries is a list
+groceries_list = ['cereal', 'milk', 'crunch', 'beer', 'tape', 'lotion', 'beer']
+if 'milk' in groceries_set:
+    print('Milk is already there')
+else:
+    print("Oh yeah, you need milk")
+
+groceries_list.append('one last thing...')
+print(groceries_list)
