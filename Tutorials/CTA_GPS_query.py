9d88e51295d9fa45996b5a3751bfb6e8f41b78b4
Vadim Blaker <vblaker@gmail.com>
2017-07-10 15:10:11 -0500

    String update

--- Tutorials/CTA_GPS_query.py
+++ Tutorials/CTA_GPS_query.py
@@ -1,7 +1,21 @@
 from urllib.request import urlopen
+from xml.etree.ElementTree import parse
+import webbrowser
 
-u = urlopen('http://ctabustracker.com/bustime/map/getBusesForRoute.jsp?route=22')
+u = urlopen('http://ctabustracker.com/bustime/map/c.jsp?route=22')
+
 data = u.read()
 fptr = open('rt22.xml', 'wb')
 fptr.write(data)
 fptr.close()
+
+doc = parse('rt22.xml')
+
+for bus in doc.findall('bus'):
+    bus_id = int(bus.findtext('id'))
+    busdir = bus.findtext('pd')
+    lat = float(bus.findtext('lat'))
+    lon = float(bus.findtext('lon'))
+    direction = bus.findtext('d')
+    print('Bus ID %d is running %s Latitude: %f Longitude: %f' % (bus_id, busdir, lat, lon))
+#    webbrowser.open('http://maps.googleapis.com/maps/api/staticmap?size=500x500&sensor=false&markers=|%f,%f' % (lat, lon))
