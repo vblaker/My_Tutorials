6a2e1d2679eb235b14fd636469da338794e950a4
Vadim Blaker <vblaker@gmail.com>
2017-09-12 15:24:55 -0500

    Initial revision of CVS parse

--- Tutorials/CSV_parse.py
+++ Tutorials/CSV_parse.py
@@ -35,3 +35,28 @@ elif (platform.system() == "Windows"):
 
 else:
     print("unknown OS")
+
+
+#PD Data Center SW
+
+# open(u'C:/Users/ra7621/Downloads/9V charging from Anker PD charger.tdc', True)        # Open File
+# export_iv(u'C:/Users/ra7621/Downloads/export.csv', {'session_id': 1L}, True)          # Export to CSV file
+
+with open('export.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=' ')
+
+    Time_stamp = []
+    VBUS_vols = []
+    VBUS_curr = []
+    VCONN_volt = []
+    VCONN_curr = []
+    CC1_volt = []
+    CC1_curr = []
+    CC2_volt = []
+    CC2_curr = []
+
+    for row in readCSV:
+        print(row)
+        Time_stamp.append(row[0])
+        VBUS_vols.append(row[1])
+        #print(VBUS_vols)
