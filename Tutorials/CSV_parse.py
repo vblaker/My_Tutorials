066b4c4b5ff7cb349c55a72779bb989aa036964c
Vadim Blaker <vblaker@gmail.com>
2017-09-13 14:23:50 -0500

    Updated parser

--- Tutorials/CSV_parse.py
+++ Tutorials/CSV_parse.py
@@ -4,6 +4,21 @@ import os
 import csv
 
 
+def IdentifyColumnHeaders(RowString):
+#Time (s),VBUS Voltage (V),VBUS Current (A),VCONN Voltage (V),VCONN Current (A),CC1 Voltage (V),CC1 Current (A),CC2 Voltage (V),CC2 Current (A)
+headers_list = ['Time (s)', 'VBUS Voltage (V)', 'VBUS Current (A)', 'VCONN Voltage (V)',
+                'VCONN Current (A)', 'CC1 Voltage (V)', 'CC1 Current (A)', 'CC2 Voltage (V)',
+                'CC2 Current (A)']
+
+    try:
+        time_stamp = row.index('Time (s)')
+        vbus_vols_idx = row.index('VBUS Voltage (V)')
+        vbus_amps_idx = row.index('VBUS Current (A)')
+        vconn_volts_idx = row.index('VCONN Voltage (V)')
+
+    except ValueError:
+        print('header Parsing Error')
+
 if (platform.system() == "Linux"):
     #os_system = str(os.system("uname -a")).split(' ')
     print(os.system("uname -a"))
@@ -43,7 +58,7 @@ else:
 # export_iv(u'C:/Users/ra7621/Downloads/export.csv', {'session_id': 1L}, True)          # Export to CSV file
 
 with open('export.csv') as csvfile:
-    readCSV = csv.reader(csvfile, delimiter=' ')
+    readCSV = csv.reader(csvfile, delimiter=',')
 
     Time_stamp = []
     VBUS_vols = []
@@ -55,8 +70,32 @@ with open('export.csv') as csvfile:
     CC2_volt = []
     CC2_curr = []
 
+    i = 0
     for row in readCSV:
-        print(row)
-        Time_stamp.append(row[0])
-        VBUS_vols.append(row[1])
-        #print(VBUS_vols)
+        #print(row)
+
+        try:
+            if any('VBUS') in row:
+                IdentifyColumnHeaders(row)
+
+
+            f = float(row[0])
+            #print(f)
+            Time_stamp.append(float(row[0]))
+            VBUS_vols.append(float(row[1]))
+            VBUS_curr.append(float(row[2]))
+            VCONN_volt.append(float(row[3]))
+            VCONN_curr.append(float(row[4]))
+            CC1_volt.append(float(row[5]))
+            CC1_curr.append(float(row[6]))
+            CC2_volt.append(float(row[7]))
+            CC2_curr.append(float(row[8]))
+            i += 1
+        except ValueError:
+            print('Skipping row {}'.format(row))
+
+
+print('The size of data is {} rows'.format(i))
+print('Time Stamps:{}'.format(Time_stamp))
+print('VBUS MAX Voltage: {}'.format(max(VBUS_vols)))
+print('VBUS Voltages:{}'.format(VBUS_vols))
