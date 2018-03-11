5466d78d4823213c777bcfce004ceec9042a830a
vblaker <vblaker@gmail.com>
2018-03-11 13:51:19 -0500

    Update

--- Tutorials/Selenium_101.py
+++ Tutorials/Selenium_101.py
@@ -1,13 +1,12 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
+import selenium.webdriver.common.keys
 
-
-driver = webdriver.Chrome("D:\Google Drive\Continuous Education\Python\My_Tutorials\selenium_drivers\chromedriver.exe")
-
-driver.set_page_load_timeout(10)
-driver.get("http://google.com")
-driver.find_element_by_name("q").send_keys("Selenium automation")
-driver.find_element_by_name("btnK").send_keys(Keys.ENTER)
-time.sleep(10)
-driver.quit()
+driver = selenium.webdriver.Chrome("D:\Google Drive\Continuous Education\Python\My_Tutorials\selenium_drivers\chromedriver.exe")
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(selenium.webdriver.common.keys.Keys.RETURN)
+assert "No results found." not in driver.page_source
+driver.close()
