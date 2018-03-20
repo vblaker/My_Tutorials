3963d91eba020891a3ad2d1dcfd2bb845645616d
vblaker <vblaker@gmail.com>
2018-03-20 17:12:01 -0500

    Update

--- Tutorials/Exceptions.py
+++ Tutorials/Exceptions.py
@@ -3,13 +3,18 @@ import sys
 while True:
     try:
         number = int(input("What's you favorite number?:\n"))
+        if number == 0:
+            raise ValueError
         print(18/number)
         errorFlag = False
         break
     except ValueError:
-        error = sys.exc_info()[0]
+        error = sys.exc_info()[1]
         print(error)
-        print("You didn't enter a number!")
+        if number == 0:
+            print('You entered a 0 but you can\'t divide by it!')
+        else:
+            print("You didn't enter a number!")
         errorFlag = True
-    if not errorFlag :
+    if not errorFlag:
         print("All is good!")
