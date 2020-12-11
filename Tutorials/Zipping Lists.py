ee325f9969976e6e82870d57d58ae0782c03d6e6
Vadim Blaker <vblaker@gmail.com>
2020-12-10 19:52:52 -0600

    Minor update

--- Tutorials/Zipping Lists.py
+++ Tutorials/Zipping Lists.py
@@ -2,10 +2,12 @@ items = ['November 30, 2016', 'Bread Gloves', 8.51]
 [date, name, price] = items
 print(name)
 
+
 def drop_first_last(grades):
     first, *middle, last = grades
     average = sum(middle) / len(middle)
     print(average)
+
 
 drop_first_last([65, 76, 98, 54, 21])
 
