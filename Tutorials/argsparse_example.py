f68955be4eb3b14b9d410b1e93bf9970f858cf87
Vadim Blaker <vblaker@gmail.com>
2021-01-24 20:10:43 -0600

    Minor update

--- Tutorials/argsparse_example.py
+++ Tutorials/argsparse_example.py
@@ -3,7 +3,7 @@ import argparse
 # Set debug parameter from the command prompt
 parser = argparse.ArgumentParser()
 parser.add_argument("-n", "--last_name", help="Last Name")
-parser.add_argument("-v", "--version", help="display version of the update utility and exit")
+parser.add_argument("-v", "--version", help="Display version of the update utility and exit")
 args = parser.parse_args()
 
 if args.last_name:
