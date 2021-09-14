e26f8a043ab20f09766a875e498282468a9bef22
Vadim Blaker <vblaker@gmail.com>
2021-09-14 18:34:43 -0500

    Update to driver.find_element

--- Tutorials/Selenium_101.py
+++ Tutorials/Selenium_101.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import selenium.webdriver.common.keys
 
-driver = selenium.webdriver.Chrome("D:\Google Drive\Continuous Education\Python\My_Tutorials\selenium_drivers\chromedriver.exe")
+driver = selenium.webdriver.Chrome("D:\Google Drive\Continuous Education\Python\Tutorials\selenium_drivers\chromedriver.exe")
 driver.get("http://www.python.org")
 assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
+elem = driver.find_element(by=By.NAME, value="q")
 elem.clear()
 elem.send_keys("pycon")
 elem.send_keys(selenium.webdriver.common.keys.Keys.RETURN)
